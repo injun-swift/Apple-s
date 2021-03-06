d3301f2bf94f352d5322bcdf21eb0e92691d2bb9
injun <ijcho6924@icloud.com>
2021-03-06 16:07:12 +0900

    first commit

---
+++ Apple's/Apple_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_sApp.swift
+//  Apple's
+//
+//  Created by injun Jo on 2021/03/06.
+//
+
+import SwiftUI
+
+@main
+struct Apple_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple's/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple's
+//
+//  Created by injun Jo on 2021/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
